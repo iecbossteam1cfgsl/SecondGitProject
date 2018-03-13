b15a8acd3eb6957774ca8b9a0a6a67a484a15518
Ricardo <Ricardo@DESKTOP-7FKN6HE>
2018-03-13 19:30:32 +0100

    third commit

--- SecondGitProject/src/Principal.java
+++ SecondGitProject/src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		Invoice in = new Invoice();
 		System.out.println("text second commit");
+		System.out.println("text third commit");
 	}
 
 }
