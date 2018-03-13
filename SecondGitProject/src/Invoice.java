880e27eb2f1a6d651f6a3890b2cd8cb676764e22
Ricardo <Ricardo@DESKTOP-7FKN6HE>
2018-03-13 19:13:16 +0100

    Add constructor class Invoice

--- SecondGitProject/src/Invoice.java
+++ SecondGitProject/src/Invoice.java
@@ -1,5 +1,11 @@
 
 public class Invoice {
-private int id;
-private String producto;
+	private int id;
+	private String producto;
+	public Invoice(int id, String producto) {
+		super();
+		this.id = id;
+		this.producto = producto;
+	}
+	
 }
