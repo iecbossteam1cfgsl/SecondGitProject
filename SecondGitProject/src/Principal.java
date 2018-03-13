a4f5b39f9ac6fc8923f6dddd0acb4ac361628332
Ricardo <Ricardo@DESKTOP-7FKN6HE>
2018-03-13 19:15:44 +0100

    Modify principal

--- SecondGitProject/src/Principal.java
+++ SecondGitProject/src/Principal.java
@@ -3,8 +3,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Invoice in = new Invoice();
+		Invoice in = new Invoice(1,"tornillo 45");
 		System.out.println("text second commit");
+		System.out.println("");
 	}
 
 }
